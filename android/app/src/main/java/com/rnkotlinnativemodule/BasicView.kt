405623b8fde0586b3d0e5ff1fbb526c7e2198ff4
Art <art@Arts-MacBook-Pro.local>
2020-01-22 11:39:48 +0700

    Implement layout setup (inflation)
    - Configure Kotlin

--- android/app/src/main/java/com/rnkotlinnativemodule/BasicView.kt
+++ android/app/src/main/java/com/rnkotlinnativemodule/BasicView.kt
@@ -1,4 +1,11 @@
 package com.rnkotlinnativemodule
 
-class BasicView {
+import android.content.Context
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+
+class BasicView(context: Context): LinearLayout(context) {
+    init {
+        LayoutInflater.from(context).inflate(R.layout.basic_view, this, true)
+    }
 }
