faa00e5582ac7ecac35f4c65574b2839a2ef6d55
Art <art@Arts-MacBook-Pro.local>
2020-01-22 12:43:48 +0700

    Create and link native module package

---
+++ android/app/src/main/java/com/rnkotlinnativemodule/RNPackage.kt
@@ -0,0 +1,20 @@
+package com.rnkotlinnativemodule
+
+import android.view.View
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ReactShadowNode
+import com.facebook.react.uimanager.ViewManager
+
+class RNPackage: ReactPackage {
+    override fun createNativeModules(reactContext: ReactApplicationContext): MutableList<NativeModule> {
+        return mutableListOf()
+    }
+
+    override fun createViewManagers(reactContext: ReactApplicationContext): MutableList<ViewManager<out View, out ReactShadowNode<*>>> {
+        return mutableListOf(
+                RNBasicView()
+        )
+    }
+}
